6991787431e881b85098d8f565dc315629c7f2e6
Yogesh Dewasi <yogeshdewasi7702@gmail.com>
2026-01-07 13:50:01 +0530

    chore: initialize react frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+     <div className="App">
+      <h1>DevCollab</h1>
+      <p>We are going to build a real developer collaboration platform step by step.</p>
+    </div>
+  );
+}
+
+export default App;
